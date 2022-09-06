406a4e39457490d4ddb860cdcbe76fc28fe84531
Kathy Kang <105574653+KatSKang@users.noreply.github.com>
2022-09-06 17:33:45 -0400

    added questions array

--- Develop/index.js
+++ Develop/index.js
@@ -1,7 +1,58 @@
 // TODO: Include packages needed for this application
+const fs = require('fs');
+const inquirer = require('inquirer');
+
 
 // TODO: Create an array of questions for user input
-const questions = [];
+//the title of my project and sections entitled Description, Table of Contents, Installation, Usage, License, Contributing, Tests, and Questions
+const questions = [
+    {
+        type: "input",
+        message: "What is the name of your project?",
+        name: "title",
+    },
+    {
+        type: "input",
+        message: "Please provide a description your project.",
+        name: "description",
+    },
+    {
+        type: "input",
+        message: "Please provide a Table of Contents for your project.",
+        name: "contents",
+    },
+    {
+        type: "input",
+        message: "What are the steps required to install your project? Provide a step-by-step description of how to get the development environment running.",
+        name: "installation",
+    },
+    {
+        type: "input",
+        message: "Provide instructions and examples for use.",
+        name: "usage",
+    },
+    {
+        type: "list",
+        message: "What licenses would you like to include for your project?",
+        name: "license",
+        choices: "MIT License, APACHE License, GNU General Public License, No license"
+    },
+    {
+        type: "input",
+        message: "Please list any collaborators or third party contributors for your project.",
+        name: "contributor",
+    },
+    {
+        type: "input",
+        message: "Add test instructions for your project?",
+        name: "test",
+    },
+    {
+        type: "input",
+        message: "Add questions for your project?",
+        name: "questions",
+    }
+];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
